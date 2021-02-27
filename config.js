1dc8e469ae187417cc6eddabf6ae3a5d1f4b42f5
wa7sh455 <79741820+wa7sh455@users.noreply.github.com>
2021-02-28 01:09:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /* Your Bot Default Prefix For Every Server */
-exports.Default_Prefix = "";
+exports.Default_Prefix = "!";
 /* Your Bot All Embeds Color */
-exports.Color = "";
-/* Your Bot Token (Secret) */
-exports.Token = "";
+exports.Color = "red";
+/* Your Bot Token (secret) */
+exports.Token = "ODE1MTE5MTk4MjQ2MjczMDI0.YDnw6Q.tMNLsqrcZgrdYyBUjb02KwXs710";
 /* Bot's Owner ID */
-exports.OwnerID = "";
+exports.OwnerID = "770219351076634624";
